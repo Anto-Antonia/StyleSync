4438226f21fa350db04ff5a4c586f6f1afb700e1
Anto-Antonia <nedeeaanto@gmail.com>
2025-08-26 16:00:12 +0300

    Finished the controller for user and role

--- StyleSync/src/main/java/com/example/StyleSync/controller/UserRoleController.java
+++ StyleSync/src/main/java/com/example/StyleSync/controller/UserRoleController.java
@@ -1,11 +1,16 @@
 package com.example.StyleSync.controller;
 
+import com.example.StyleSync.dto.request.role.RoleRequest;
 import com.example.StyleSync.dto.request.user.UserRequest;
 import com.example.StyleSync.dto.request.user.UserUpdateUsername;
+import com.example.StyleSync.dto.response.product.FavoriteProductResponse;
+import com.example.StyleSync.dto.response.role.RoleResponse;
 import com.example.StyleSync.dto.response.user.UserResponse;
+import com.example.StyleSync.entity.Role;
 import com.example.StyleSync.service.UserRoleService;
 import jakarta.validation.Valid;
 import lombok.Getter;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -48,5 +53,57 @@ public class UserRoleController {
     public ResponseEntity<Void> updateUserUsername(@PathVariable Integer id, @RequestBody @Valid UserUpdateUsername userUpdateUsername ){
         service.updateUserUsername(id, userUpdateUsername);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PostMapping("/{userId}/addToFav/{itemId}")
+    public ResponseEntity<String> addItemToFavorite(@PathVariable Integer userId, @PathVariable Integer itemId){
+        service.addItemToFavorite(userId, itemId);
+        return ResponseEntity.ok("Item added to favorites");
+    }
+
+    @PostMapping("/{userId}/removeItem/{itemId}")
+    public ResponseEntity<String> removeItemFromFavorite(@PathVariable Integer userId, @PathVariable Integer itemId){
+        service.removeItemFromFavorite(userId, itemId);
+        return ResponseEntity.ok("Item removed from favorites");
+    }
+
+    @GetMapping("/getFavProducts/{userId}")
+    public ResponseEntity<List<FavoriteProductResponse>> getFavoriteProducts(@PathVariable Integer userId){
+        List<FavoriteProductResponse> responses = service.getFavoriteProducts(userId);
+        return ResponseEntity.ok().body(responses);
+    }
+
+    @DeleteMapping("/{userId}")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<String> deleteUser(@PathVariable Integer userId){
+        service.deleteUser(userId);
+        return ResponseEntity.ok("User with the id: " + userId + "has been deleted");
+    }
+
+    @PostMapping("/addRole")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<Role> addRole(@RequestBody RoleRequest request){
+        Role role = service.addRole(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(role);
+    }
+
+    @PostMapping("/role/addRoleToUser/{userId}")
+    public ResponseEntity<Void> addRoleToUser(@PathVariable @Valid Integer userId, @RequestBody String name){
+        service.addRoleToUser(userId, name);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("role")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<RoleResponse> getRole(@PathVariable Integer id){
+        RoleResponse response = service.getRole(id);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/role/{id}")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<String> deleteRole(@PathVariable @Valid Integer id){
+        service.deleteRole(id);
+        return ResponseEntity.ok("The role with id " + id + " has been deleted");
     }
 }
