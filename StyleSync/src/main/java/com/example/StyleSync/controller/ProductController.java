f683950bad853a749fb9de3d6cb6014fd9c66ef4
Anto-Antonia <nedeeaanto@gmail.com>
2025-08-26 17:09:40 +0300

    Finished product controller

--- StyleSync/src/main/java/com/example/StyleSync/controller/ProductController.java
+++ StyleSync/src/main/java/com/example/StyleSync/controller/ProductController.java
@@ -1,14 +1,18 @@
 package com.example.StyleSync.controller;
 
 import com.example.StyleSync.dto.request.product.ProductRequest;
+import com.example.StyleSync.dto.request.product.UpdateProductRequest;
+import com.example.StyleSync.dto.response.product.ProductResponse;
 import com.example.StyleSync.entity.Product;
 import com.example.StyleSync.service.ProductService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -20,8 +24,36 @@ public class ProductController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<Product> addProduct(@RequestBody ProductRequest productRequest){
         Product product = service.addProduct(productRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable @Valid Integer id){
+        ProductResponse response = service.getProductById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductResponse>> getAllProduct(){
+        List<ProductResponse> responses = service.getAllProducts();
+        return ResponseEntity.ok().body(responses);
+    }
+
+    @PatchMapping("/products/{id}")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<String> updateProduct(@PathVariable Integer id, @RequestBody UpdateProductRequest updateProductRequest){
+        service.updateProduct(id, updateProductRequest);
+        return ResponseEntity.ok("The product has been updated.");
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<String> removeProduct(@PathVariable @Valid Integer id){
+        service.removeProduct(id);
+        return ResponseEntity.ok("The product with id " + id + "has been removed");
     }
 }
